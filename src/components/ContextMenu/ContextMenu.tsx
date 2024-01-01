3e64eb300c5c96047e6e0a9b1a650401ea1a6757
jayvanandel07 <j.vanandel07@gmail.com>
2024-01-01 16:00:20 +0530

    Context menu style and functionality

--- src/components/ContextMenu/ContextMenu.tsx
+++ src/components/ContextMenu/ContextMenu.tsx
@@ -1,25 +1,45 @@
-import React, { FC } from 'react'
-import styles from "./ContextMenu.module.css";
-import { useContextMenu } from '../../contexts/ContextMenuContext';
+import React, { FC, MutableRefObject } from "react";
+import styles from "./ContextMenu.module.scss";
+import { useContextMenu } from "../../contexts/ContextMenuContext";
+import { CanvasSlideInstance } from "../Slide/CanvasSlide";
 
-
-
-
-const ContextMenu: FC = () =>
-{   
-    const { isVisible, position, hideContextMenu } = useContextMenu();
-    
-    if (!isVisible) return null;
-
-    const { top, left } = position;
-
-    return (
-        <div className={`${styles.customMenu}`} style={{display: `${isVisible ? "block" : "none"}`,top:top,left:left}} >
-            <ul>
-                <li onClick={() => hideContextMenu()}>Hide Context Menu</li>
-            </ul>
-        </div>
-    );
+interface ContextMenuProps {
+  canvasRef: MutableRefObject<CanvasSlideInstance>;
 }
+const ContextMenu: FC<ContextMenuProps> = ({ canvasRef }) => {
+  const { isVisible, position, hideContextMenu } = useContextMenu();
 
-export default ContextMenu
+  if (!isVisible) return null;
+
+  const { top, left } = position;
+
+  return (
+    <div
+      className={`${styles.customMenu}`}
+      style={{
+        display: `${isVisible ? "block" : "none"}`,
+        top: top,
+        left: left,
+      }}
+    >
+      <ul>
+        <li
+          onClick={() => {
+            canvasRef.current?.handler?.groupObjects();
+          }}
+        >
+          Group
+        </li>
+        <li
+          onClick={() => {
+            canvasRef.current?.handler?.swapActiveObjects();
+          }}
+        >
+          Swap
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default ContextMenu;
