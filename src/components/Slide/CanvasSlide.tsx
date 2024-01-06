8a735b1a6a50042f0097d5f283467cea2491daa4
Mohamed Aslam <mohamedaslamcodes@gmail.com>
2024-01-06 11:20:11 +0530

    disable context menu

--- src/components/Slide/CanvasSlide.tsx
+++ src/components/Slide/CanvasSlide.tsx
@@ -1,10 +1,10 @@
 import React, {
-  MutableRefObject,
-  forwardRef,
-  useEffect,
-  useImperativeHandle,
-  useRef,
-  useState,
+    MutableRefObject,
+    forwardRef,
+    useEffect,
+    useImperativeHandle,
+    useRef,
+    useState,
 } from "react";
 import styles from "./CanvasSlide.module.scss";
 import { fabric } from "fabric";
@@ -12,53 +12,53 @@ import { CanvasHandler } from "./CanvasHandler";
 import { useContextMenu } from "../../contexts/ContextMenuContext";
 
 interface SlideProps {
-  ref: MutableRefObject<CanvasSlideInstance>;
+    ref: MutableRefObject<CanvasSlideInstance>;
 }
 
 export interface CanvasSlideInstance {
-  canvas: fabric.Canvas | null;
-  handler?: CanvasHandler;
+    canvas: fabric.Canvas | null;
+    handler?: CanvasHandler;
 }
 
 const CanvasSlide = forwardRef<CanvasSlideInstance, Omit<SlideProps, "ref">>(
-  function CanvasSlide(props, ref) {
-    const canvasRef = useRef<CanvasSlideInstance>({
-      canvas: null,
-    });
+    function CanvasSlide(props, ref) {
+        const canvasRef = useRef<CanvasSlideInstance>({
+            canvas: null,
+        });
 
-    const { showContextMenuAtMousePosition, hideContextMenu } =
-      useContextMenu();
+        const { showContextMenuAtMousePosition, hideContextMenu } =
+            useContextMenu();
 
-    useEffect(() => {
-      canvasRef.current.canvas = new fabric.Canvas("tne-canvas-id", {
-        width: 850,
-        height: 540,
-      });
+        useEffect(() => {
+            canvasRef.current.canvas = new fabric.Canvas("tne-canvas-id", {
+                width: 850,
+                height: 540,
+            });
 
-      canvasRef.current.handler = new CanvasHandler({
-        canvas: canvasRef.current.canvas as fabric.Canvas,
-        onAdd: (obj) => {},
-        onClick: () => {},
-      });
-    }, [canvasRef.current.canvas]);
+            canvasRef.current.handler = new CanvasHandler({
+                canvas: canvasRef.current.canvas as fabric.Canvas,
+                onAdd: (obj) => {},
+                onClick: () => {},
+            });
+        }, [canvasRef.current.canvas]);
 
-    useImperativeHandle(ref, () => ({
-      canvas: canvasRef.current.canvas,
-      handler: canvasRef.current.handler,
-    }));
+        useImperativeHandle(ref, () => ({
+            canvas: canvasRef.current.canvas,
+            handler: canvasRef.current.handler,
+        }));
 
-    return (
-      <div
-        className={styles["slide-container"]}
-        onContextMenu={(e) => {
-          showContextMenuAtMousePosition(e);
-        }}
-        onClick={hideContextMenu}
-      >
-        <canvas id="tne-canvas-id" className={styles.canvas} />
-      </div>
-    );
-  }
+        return (
+            <div
+                className={styles["slide-container"]}
+                onContextMenu={(e) => {
+                    // showContextMenuAtMousePosition(e);
+                }}
+                onClick={hideContextMenu}
+            >
+                <canvas id="tne-canvas-id" className={styles.canvas} />
+            </div>
+        );
+    }
 );
 
 export default CanvasSlide;
